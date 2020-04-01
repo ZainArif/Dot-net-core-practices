4f36c3a9f7a392aa9b87c2b63301584c3ac6151c
Zain Arif <zainarif14197@gmail.com>
2020-04-01 13:12:22 +0500

    Repositories & Dtos

--- Parky/ParkyAPI/Startup.cs
+++ Parky/ParkyAPI/Startup.cs
@@ -12,6 +12,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using ParkyAPI.Data;
+using ParkyAPI.Repository;
+using ParkyAPI.Repository.IRepository;
+using AutoMapper;
+using ParkyAPI.ParkyMapper;
 
 namespace ParkyAPI
 {
@@ -29,6 +33,10 @@ namespace ParkyAPI
         {
             services.AddDbContext<ApplicationDbContext>
                 (options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<INationalParkRepository, NationalParkRepository>();
+
+            services.AddAutoMapper(typeof(ParkyMappings));
             services.AddControllers();
         }
 
